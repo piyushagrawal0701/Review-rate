638e1ea390b2eaf5e3b3a7fad6bccb7a38b8004d
piyushagrawal0701 <agrawal777piyush@gmail.com>
2026-06-18 19:00:11 +0530

    Scroll Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,12 +7,14 @@ import CompanyDetails from "./pages/CompanyDetails";
 import Navbar from "./components/common/Navbar";
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/common/Footer";
+import ScrollToTop from "./components/common/ScrollToTop";
 
 function App() {
   return (
     <>
       <Navbar />
       <Toaster position="top-right" />
+      <ScrollToTop/>
       <Routes>
         <Route path="/" element={<Home />} />
 
